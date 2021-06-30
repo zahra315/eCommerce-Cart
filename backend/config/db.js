58c9efe9f5fee52e6f9ef8b6b28e2d163bd87ee1
Zahra Seyedi <za.seyedi@gmail.com>
2021-06-29 22:51:15 -0700

    MongoDB connection SUCCESS

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    console.log("MongooDB connecion SUCCESS");
+    console.log("MongoDB connection SUCCESS");
   } catch (error) {
     console.error("MongoDB connection FAIL");
     process.exit(1);
