8542cc446e41a066fbd4da8cea078e8c2b08cb6d
Zahra Seyedi <za.seyedi@gmail.com>
2021-06-29 22:50:11 -0700

    producct schema

---
+++ backend/models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  countInStock: {
+    type: Number,
+    require: true,
+  },
+  imageUrl: {
+    type: String,
+    require: true,
+  },
+});
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
