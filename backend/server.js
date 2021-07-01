8de439c4384df570c73f70054bbee0b4dbf8efef
Zahra Seyedi <za.seyedi@gmail.com>
2021-06-30 19:17:23 -0700

    Building out Routes and Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+
+// const multer = require("multer");
+// const multerMid = multer({
+//   storage: multer.memoryStorage(),
+//   limits: {
+//     fieldSize: 5 * 1024 * 1024,
+//   },
+// });
 
 connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
