e722db2273dfea51ff3ff768295aa6662cda620c
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-03 09:24:49 -0700

    use Selector to GET products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,35 @@
 import "./HomeScreen.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 //Components
 import Product from "../components/Product";
 
+//Actions
+import { getProducts as listProducts } from "../redux/actions/productActions";
+
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts);
+  }, [dispatch]);
+
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Products</h2>
+
       <div className="homescreen__products">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((Product) => <Product />)
+        )}
       </div>
     </div>
   );
