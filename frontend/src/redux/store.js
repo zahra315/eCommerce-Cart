7347a22e03ff943dd0a8a3114cdc4000555bf33d
Zahra Seyedi <za.seyedi@gmail.com>
2021-07-20 21:53:25 -0700

    check the cart value exist in localStorage

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -17,8 +17,20 @@ const reducer = combineReducers({
 
 const middleware = [thunk];
 
+//check if we can find the cart value in our localStorage
+const cartFromLocalStorage = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : [];
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartFromLocalStorage,
+  },
+};
+
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
